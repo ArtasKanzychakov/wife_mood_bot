160b39c5bf0663d8cd0ef95d61037cfa2bbaf755
ArtasKanzychakov <Artasmovie@yandex.ru>
2025-07-22 12:56:34 +0700

    Update crud.py

--- database/crud.py
+++ database/crud.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base, User
 from config.settings import Config
+from datetime import datetime  # Добавлен импорт
 
 engine = create_engine(Config.POSTGRES_URL)
 SessionLocal = sessionmaker(bind=engine)
@@ -22,7 +23,7 @@ async def create_user(db, telegram_id: int, name: str, age: int, birth_date, zod
         name=name,
         age=age,
         birth_date=birth_date,
-        zodiac=zinc(zodiac),
+        zodiac=zodiac,  # Убрана несуществующая функция zinc()
         registration_date=datetime.now()
     )
     db.add(user)
